e280712343bb781c2b6bd09b8ce6e3c53aa4bb91
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-10-17 20:19:48 -0400

    Refactor ClassInstanceDistribution

--- ClassInstanceDistribution.py
+++ ClassInstanceDistribution.py
@@ -1,43 +1,22 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-data_test_1 = pd.read_csv('Assig1-Dataset/test_with_label_1.csv')
-data_test_2 = pd.read_csv('Assig1-Dataset/test_with_label_2.csv')
-data_train_1 = pd.read_csv('Assig1-Dataset/train_1.csv')
-data_train_2 = pd.read_csv('Assig1-Dataset/train_2.csv')
-data_val_1 = pd.read_csv('Assig1-Dataset/val_1.csv')
-data_val_2 = pd.read_csv('Assig1-Dataset/val_2.csv')
 
-data_test_1.iloc[:, -1].value_counts().sort_index()\
-    .plot(kind='bar', title='Distribution of Letters by Index in test_with_label_1')
-fig = plt.gcf()
-fig.savefig('Output/Initial_Distributions/distribution_test1.pdf')
-plt.show()
-data_test_2.iloc[:, -1].value_counts().sort_index()\
-    .plot(kind='bar', title='Distribution of Letters by Index in test_with_label_2')
-fig = plt.gcf()
-fig.savefig('Output/Initial_Distributions/distribution_test2.pdf')
-plt.show()
-data_train_1.iloc[:, -1].value_counts().sort_index()\
-    .plot(kind='bar', title='Distribution of Letters by Index in train_1')
-fig = plt.gcf()
-fig.savefig('Output/Initial_Distributions/distribution_train1.pdf')
-plt.show()
-data_train_2.iloc[:, -1].value_counts().sort_index()\
-    .plot(kind='bar', title='Distribution of Letters by Index in train_2')
-fig = plt.gcf()
-fig.savefig('Output/Initial_Distributions/distribution_train2.pdf')
-plt.show()
-data_val_1.iloc[:, -1].value_counts().sort_index()\
-    .plot(kind='bar', title='Distribution of Letters by Index in val_1')
-fig = plt.gcf()
-fig.savefig('Output/Initial_Distributions/distribution_val1.pdf')
-plt.show()
-data_val_2.iloc[:, -1].value_counts().sort_index()\
-    .plot(kind='bar', title='Distribution of Letters by Index in val_2')
-fig = plt.gcf()
-fig.savefig('Output/Initial_Distributions/distribution_val2.pdf')
-plt.show()
+def print_distribution(dataset_path, dataset_name):
+    dataset = pd.read_csv(dataset_path)
+    dataset.iloc[:, -1].value_counts().sort_index()\
+        .plot(kind='bar', title=f'Distribution of Letters by Index in {dataset_name}')
+    fig = plt.gcf()
+    fig.savefig(f'Output/Initial_Distributions/distribution_{dataset_name}.pdf')
+    plt.show()
+
+
+print_distribution('Assig1-Dataset/test_with_label_1.csv', 'Test Data Set 1')
+print_distribution('Assig1-Dataset/test_with_label_2.csv', 'Test Data Set 2')
+print_distribution('Assig1-Dataset/train_1.csv', 'Training Data Set 1')
+print_distribution('Assig1-Dataset/train_2.csv', 'Training Data Set 2')
+print_distribution('Assig1-Dataset/val_1.csv', 'Validation Data Set 1')
+print_distribution('Assig1-Dataset/val_2.csv', 'Validation Test Data Set 2')
 
 
 
