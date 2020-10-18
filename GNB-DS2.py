e65bd23e8aacec315f4c662f9658be2dfeafb347
Daniel Wiktorczyk <danielwiktorczyk@gmail.com>
2020-10-18 00:14:56 -0400

    Use Training, Validation, and Test Sets Properly in Remaining Models

--- GNB-DS2.py
+++ GNB-DS2.py
@@ -3,42 +3,54 @@ from sklearn.naive_bayes import GaussianNB
 from sklearn.metrics import accuracy_score, confusion_matrix, precision_recall_fscore_support
 import csv
 
-data_train = pd.read_csv('Assig1-Dataset/train_2.csv')
-data_test = pd.read_csv('Assig1-Dataset/test_with_label_2.csv')
-data_valid = pd.read_csv('Assig1-Dataset/val_2.csv')
-
+output_results_file_path = 'Output/GNB-DS2.csv'
 letters = ['pi', 'alpha', 'beta', 'sigma', 'gamma', 'delta', 'lambda', 'omega', 'mu', 'xi']
 
-train_features = data_train.iloc[:, :-1]
-train_target = data_train.iloc[:, -1]
-test_features = data_test.iloc[:, :-1]
-test_target = data_test.iloc[:, -1]
-valid_features = data_valid.iloc[:, :-1]
-valid_target = data_valid.iloc[:, -1]
+# Read our data sets
+data_train = pd.read_csv('Assig1-Dataset/train_2.csv')
+data_valid = pd.read_csv('Assig1-Dataset/val_2.csv')
+data_test = pd.read_csv('Assig1-Dataset/test_with_label_2.csv')
 
+# We'll combine our training and validation data since we won't be tweaking our hyper parameters yet
+data_valid.columns = data_train.columns
+combined_training_set = pd.concat([data_train, data_valid], ignore_index=True, axis=0)
+testing_set = data_test
+
+# Define the features and target sets
+combined_training_set_features = combined_training_set.iloc[:, :-1]
+combined_training_set_target = combined_training_set.iloc[:, -1]
+testing_set_features = data_test.iloc[:, :-1]
+testing_set_target = data_test.iloc[:, -1]
+
+# Define the classifier used
 classifier = GaussianNB()
-classifier.fit(train_features, train_target)
 
-valid_prediction = classifier.predict(valid_features)
+# Fit accordingly to our combined training and validation set
+classifier.fit(combined_training_set_features, combined_training_set_target)
 
-valid_confusion = confusion_matrix(valid_target, valid_prediction)
-p1, r1, f1, _ = precision_recall_fscore_support(valid_target, valid_prediction)
-p2, r2, f2, _ = precision_recall_fscore_support(valid_target, valid_prediction, average='weighted')
-p3, r3, f3, _ = precision_recall_fscore_support(valid_target, valid_prediction, average='macro')
-valid_accuracy = accuracy_score(valid_target, valid_prediction)
+# Finally, predict the test set the best estimator that GridSearch has scored during training
+test_prediction = classifier.predict(testing_set_features)
 
-file = open("Output/GNB-DS2.csv", 'w', encoding='utf8')
+# Metrics
+test_confusion = confusion_matrix(testing_set_target, test_prediction)
+p1, r1, f1, _ = precision_recall_fscore_support(testing_set_target, test_prediction, zero_division='warn')
+p2, r2, f2, _ = precision_recall_fscore_support(testing_set_target, test_prediction, average='weighted')
+p3, r3, f3, _ = precision_recall_fscore_support(testing_set_target, test_prediction, average='macro')
+test_accuracy = accuracy_score(testing_set_target, test_prediction)
+
+# Write results and metrics to file
+file = open(output_results_file_path, 'w', encoding='utf8')
 writer = csv.writer(file, quotechar='"', quoting=csv.QUOTE_ALL, lineterminator='\n')
 count = 0
-for x in valid_prediction:
+for x in test_prediction:
     writer.writerow([count, x])
     count += 1
 writer.writerow("")
 writer.writerow("")
 writer.writerow(["Confusion Matrix"])
 writer.writerow(letters)
-for x in range(letters.__len__()):
-    writer.writerow(valid_confusion[x])
+for x in range(len(letters)):
+    writer.writerow(test_confusion[x])
 writer.writerow("")
 writer.writerow("")
 writer.writerow(["Precision"])
@@ -52,6 +64,6 @@ writer.writerow(["F1-Measures"])
 writer.writerow(letters)
 writer.writerow(f1)
 writer.writerow("")
-writer.writerow(['Accuracy', valid_accuracy])
+writer.writerow(['Accuracy', test_accuracy])
 writer.writerow(['F1-Macro', f3])
 writer.writerow(['F1-Weighted', f2])
